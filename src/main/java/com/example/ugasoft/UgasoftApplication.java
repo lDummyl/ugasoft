7f6ae9eaa16b46600f1a407725bec93697617b3b
Dima Ludov <lDummyl>
2019-04-17 22:50:40 +0300

    Initial commit

---
+++ src/main/java/com/example/ugasoft/UgasoftApplication.java
@@ -0,0 +1,13 @@
+package com.example.ugasoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UgasoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UgasoftApplication.class, args);
+	}
+
+}
